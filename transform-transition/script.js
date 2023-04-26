86e5ee66fd4c4c4f4a9b9cf96c92f612cebb3d6f
Victor Zuluaga <zrvictor@outlook.com>
2023-04-25 23:34:03 -0500

    created scroll spy behavioral

---
+++ transform-transition/script.js
@@ -0,0 +1,18 @@
+const section = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  section.forEach(sec => {
+    let myTop = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (myTop >= offset && myTop < (offset + height)) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+      })
+    }
+  })
+}
