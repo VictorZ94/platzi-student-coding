83158c9d82bba89d0c51b36285c74046ce5a9e10
Victor Zuluaga <zrvictor@outlook.com>
2023-05-08 18:39:07 -0500

    minify tailwind css

--- platzi-travel-tailwind/tailwind.config.js
+++ platzi-travel-tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 /* @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./public/index.html", "./public/**/*.{html,js}"],
   darkMode: 'class',
   theme: {
     fontFamily: { 
