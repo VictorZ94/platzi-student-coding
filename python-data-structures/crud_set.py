0d2a50c19b41edebcec77c398b77d164fad3fcf5
Victor Zuluaga <zrvictor@outlook.com>
2023-06-28 20:12:47 -0500

    crud operators set data structures in python

---
+++ python-data-structures/crud_set.py
@@ -0,0 +1,25 @@
+set_countries = {'col', 'mex', 'bol', 'col'}
+size = len(set_countries)
+print(size)
+
+print('col' in set_countries)
+print('pe' in set_countries)
+
+# add Añade un elemento. recibe un string
+set_countries.add('pe')
+print(set_countries)
+set_countries.add('pe')
+print(set_countries)
+
+# update Añade cualquier tipo de objeto iterable como: listas, tuplas.
+set_countries.update(('nz', 'aus'))
+set_countries.update({'es', 'br'})
+print(set_countries)
+
+# remove() Elimina un elemento y si este no existe lanza el error “keyError”.
+# set_countries.remove('es')
+print(set_countries)
+
+# discard(): Elimina un elemento y si ya existe no lanza ningún error.
+set_countries.discard('es')
+print(set_countries)
