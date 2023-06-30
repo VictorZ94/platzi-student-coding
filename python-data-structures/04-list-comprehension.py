f5a762570d4fc813a0de5a26d6ea778f7e52fd34
Victor Zuluaga <zrvictor@outlook.com>
2023-06-30 11:49:03 -0500

    learning about list comprehensions

--- python-data-structures/04-list-comprehension.py
+++ python-data-structures/04-list-comprehension.py
@@ -1,14 +1,23 @@
 import time
+iterations = 11
 start_time = time.time()
 number = []
-for element in range(1, 11):
+for element in range(iterations):
     number.append(element * 2)
-print(number)
 end_time = time.time()
 print(f'tiempo transcurrido con for: {end_time - start_time}')
 
 start_time2 = time.time()
-num_2 = [el * 2 for el in range(1, 11)]
-print(num_2)
+num_2 = [el * 2 for el in range(iterations)]
 end_time2 = time.time()
 print(f'tiempo transcurrido con List comprehension: {end_time2 - start_time2}')
+
+#crear a list con los primeros 100 numero pares
+number = []
+for num in range(1, 100):
+    if num % 2 == 0:
+        number.append(num)
+print(number)
+
+# List comprehenstion multi-line
+[(num,print(f"{num}, {num*2}")) for num in range(1, 10) if num % 2 == 0]
