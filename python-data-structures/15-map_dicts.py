a9a329731d34a6f79f26d7637ede628d648bf883
Victor Zuluaga <zrvictor@outlook.com>
2023-07-10 19:39:17 -0500

    looping a dict iterable

--- python-data-structures/15-map_dicts.py
+++ python-data-structures/15-map_dicts.py
@@ -7,3 +7,14 @@ items =[
 prices = list(map(lambda x: x.get('price'), items))
 print(prices)
 
+def add_taxes(item):
+    item['taxes'] = item['price'] * .19
+    return item
+
+
+prices_and_taxes = list(map(add_taxes, items))
+print(prices_and_taxes)
+
+# new_prices_and_taxes = list(map( lambda x: x|{'tax': x['price']*0.19}, items))
+new_prices_and_taxes = list(map(lambda item: {**item, **{"tax": item["price"]*.19}}, items)) #it's the same to the line above
+print(new_prices_and_taxes)
