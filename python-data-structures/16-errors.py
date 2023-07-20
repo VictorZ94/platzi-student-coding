5685751e2187a55a77001f824fe040ba751d7de6
Victor Zuluaga <zrvictor@outlook.com>
2023-07-19 19:37:46 -0500

    knowning the most comun python errors

---
+++ python-data-structures/16-errors.py
@@ -0,0 +1,12 @@
+#print()? Error: SyntaxError: invalid syntax
+#print(0 / 0) #Error: ZeroDivisionError: division by zero
+#print(result) #Error: NameError: name 'result' is not defined
+
+suma = lambda x, y: x + y * 2
+#assert suma(2, 2) == 4 #AssertionError
+print("Hola 2")
+
+# own error messages
+age = 19
+if age < 18:
+    raise Exception("Don't let us younger 18 years old")
