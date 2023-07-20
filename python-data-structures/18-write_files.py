fd51a27c09384497d94301085cbe162e0cd50a9f
Victor Zuluaga <zrvictor@outlook.com>
2023-07-20 15:43:14 -0500

    updated files

--- python-data-structures/18-write_files.py
+++ python-data-structures/18-write_files.py
@@ -1,4 +1,4 @@
 # this is the way more recommended to open a file in python
-with open('./text.txt', 'r+') as file:
+with open('./text.txt', 'w') as file:
     print(file.writable())
     file.write("My code is better tha another file")
